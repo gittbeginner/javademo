8914c5bfd8ec8894951cd4c7e4f243a7dc4fb004
gittbeginner <pandeyg1963@gmail.com>
2021-01-12 20:53:41 +0530

    Initial Commit

---
+++ src/com/flipkart/demo/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author ravi
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Inside the App.java");
+
+	}
+
+}
---
+++ src/com/flipkart/demo/Child.java
@@ -0,0 +1,7 @@
+package com.flipkart.demo;
+
+public class Child extends App {
+	{
+		System.out.println("Inside Child class");
+	}
+}
